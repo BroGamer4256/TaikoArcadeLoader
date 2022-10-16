0176ab266790be722163d3b66a66fc9dc9fb70df
BroGamer <64546358+BroGamer4256@users.noreply.github.com>
2022-10-17 05:28:08 +1300

    Add window res option, up limit to 9k?

--- plugins/8.18/dllmain.c
+++ plugins/8.18/dllmain.c
@@ -17,44 +17,44 @@ PreInit () {
 	if (readConfigBool (config, "unlock_songs", true)) WRITE_MEMORY (ASLR (0x140314E8D, handle), u8, 0xB0, 0x01); // Unlock songs
 	if (readConfigBool (config, "shared_audio", true)) WRITE_MEMORY (ASLR (0x140692E17, handle), u8, 0xEB);       // Shared audio
 	// Remove song limit
-	WRITE_MEMORY (ASLR (0x140313726, handle), u8, 0x28, 0x23);
+	WRITE_MEMORY (ASLR (0x140313726, handle), i32, 9000);
 	// Remove for with server
-	WRITE_MEMORY (ASLR (0x1402F39E6, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1402F3AB0, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1402F3BE4, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x14030643B, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140306507, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1403065D3, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1403066FB, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1403067C7, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140306893, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x14030698B, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140313666, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140313726, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1403139F4, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140313B04, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140313C24, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140313CF4, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1403140C4, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1403147AA, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140225FB6, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140226146, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140314DCC, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140314EC9, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140338E2C, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1400EE0A4, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1400EE8B5, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1400EEDA6, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140315608, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x14034A7EB, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x1402F3CB3, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140314059, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140226063, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x14022609F, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140226296, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140306A2E, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140314F46, handle), i32, 4000);
-	WRITE_MEMORY (ASLR (0x140314F97, handle), i32, 4000);
+	WRITE_MEMORY (ASLR (0x1402F39E6, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1402F3AB0, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1402F3BE4, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x14030643B, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140306507, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1403065D3, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1403066FB, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1403067C7, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140306893, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x14030698B, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140313666, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140313726, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1403139F4, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140313B04, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140313C24, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140313CF4, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1403140C4, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1403147AA, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140225FB6, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140226146, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140314DCC, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140314EC9, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140338E2C, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1400EE0A4, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1400EE8B5, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1400EEDA6, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140315608, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x14034A7EB, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x1402F3CB3, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140314059, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140226063, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x14022609F, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140226296, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140306A2E, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140314F46, handle), i32, 9000);
+	WRITE_MEMORY (ASLR (0x140314F97, handle), i32, 9000);
 
 	if (!readConfigBool (config, "vsync", false)) WRITE_MEMORY (ASLR (0x140517339, handle), u8, 0xBA, 0x00, 0x00, 0x00, 0x00, 0x90); // Disable VSync
 	// Save settings cross session without F:/ and G:/ drive
@@ -79,4 +79,14 @@ PreInit () {
 
 	INSTALL_HOOK_DYNAMIC (qrVtable1, amHandle + 0x1BA00);
 	INSTALL_HOOK_DYNAMIC (qrReadFromCOM1, amHandle + 0x1BC20);
+
+	toml_table_t *windowResSection = openConfigSection (config, "windowRes");
+	if (windowResSection) {
+		i32 windowResX = readConfigInt (windowResSection, "x", 0);
+		i32 windowResY = readConfigInt (windowResSection, "y", 0);
+		if (windowResX > 0 && windowResY > 0) {
+			WRITE_MEMORY (ASLR (0x14035FC5B, handle), u32, windowResX);
+			WRITE_MEMORY (ASLR (0x14035FC62, handle), u32, windowResY);
+		}
+	}
 }
